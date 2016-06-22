2608a1ef662ed74fb034d2c9fc4be2be564dd455
Hsun-Yi <hhsiehster@gmail.com>
2016-06-22 19:05:15 -0400

    added sql files and modified nyp.py

--- nyp.py
+++ nyp.py
@@ -1,7 +1,35 @@
-import xml.etree.ElementTree as ET
+try:
+    import xml.etree.cElementTree as ET
+except ImportError:
+    import xml.etree.ElemeneTree as ET
+    
+
+#import xml.etree.ElementTree as ET
+
 import untangle
+
 tree = ET.parse('complete.xml')
-root = tree.getroot()
-print(root[0])
+programs = tree.getroot()
+programs.tag, programs.attrib
 
+#for child_of_root in root:
+#    for grands in child_of_root:
+#        for greats in grands:
+#            print greats.tag + ": " +  greats.text
+            
+programs = tree.findall("program")
+for program in programs:
+    print program.find("programID").text
+    print program.find("orchestra").text
+    print program.find("season").text
+ 
 
+import sqlite3
+conn = sqlite3.connect("program.sqlite3")
+curs = conn.cursor()
+curs.execute("insert into program values (?, ?, ?)", (1,"NYO","2030-2031"))
+curs.rowcount
+conn.commit()    
+    
+for row in curs.fetchall():
+    print(row)
---
+++ program-create.sql
@@ -0,0 +1,5 @@
+create table if not exists program (
+  programID text,
+  orchestra text,
+  season text
+);
